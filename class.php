ce5b6a8aa09884792bfc8a1199383362f1072b90
Nejc <nejc.stibilj01@gmail.com>
2020-05-29 21:11:55 +0200

    +class constructor

--- class.php
+++ class.php
@@ -1,37 +1,49 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+
+  <?php
+  class Book{
+    var $title;
+    var $author;
+    var $pages;
+
+    function __construct($title,$author,$pages ){
+
+      $this->title = $title;
+      $this->author = $author;
+      $this->pages = $pages;
 
 
-    <?php
 
-    class Book{
-
-      var $title;
-      var $author;
-      var $pages;
-
+      echo "Obj made <br>";
     }
 
-    $book1 = new Book;
+  }
 
-    $book1->title = "Povodni Moz";
-    $book1->author = "Presern";
-    $book1->pages = 400;
+/*
+  $book1 = new Book();
+  $book1->title = "Povodni Moz";
+  $book1->author = "Presern";
+  $book1->pages = 400;
 
-    $book2 = new Book;
+  $book2 = new Book();
+  $book2->title = "The book about books";
+  $book2->author = "T. Moore";
+  $book2->pages = 170;
 
-    $book2->title = "The book about books";
-    $book2->author = "T. Moore";
-    $book2->pages = 170;
+  */
+  $book3 = new Book("Naslovna", "Pavcek", 700);
 
-    echo "$book1->title".": "."$book2->author";
+  echo "$book3->title";
 
-     ?>
+  echo "$book1->title".": "."$book2->author";
 
-  </body>
+  ?>
+
+</body>
 </html>
