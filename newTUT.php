2ec020c5d2351040358c361d8047fc382449f319
Nejc <nejc.stibilj01@gmail.com>
2020-06-01 14:06:57 +0200

    +BS4+php

--- newTUT.php
+++ newTUT.php
@@ -16,8 +16,8 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a href="#" class="navbar-brand">PajoCorp</a>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
+    <a href="#" class="navbar-brand"><img id="navIcon" src="img/phpLogo.svg"></a>
     <button class="navbar-toggler" data-toggle="collapse" data-target="#navMenu">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -31,12 +31,49 @@
     </div>
   </nav>
 
-
   <?php
   $typeOf = "bloody";
 
   include "hdr2.html";
   ?>
+
+
+  <div class="container-fluid" id="plan">
+    <div class="row">
+      <!-- start cols -->
+      <div class="col-md-3 col-xl">
+        <p>Basic Principles of PHP</p>
+      </div>
+      <div class="col-md-3 col-xl">
+        <p>PHP7 / Apache / MySQL Setup</p>
+      </div>
+      <div class="col-md-3 col-xl">
+        <p>Code Syntax</p>
+      </div>
+      <div class="col-md-3 col-xl">
+      <p>Procedural & OOP</p>
+      </div>
+      <div class="col-md col-xl">
+      <p>Dynamic Pages</p>
+      </div>
+      <div class="col-md col-xl">
+      <p>Form Submissions / Superglobals</p>
+      </div>
+      <div class="col-md col-xl">
+      <p>Working with Databases</p>
+      </div>
+      <div class="col-md col-xl">
+      <p>Mini Projects (Contact form, User Login)</p>
+      </div>
+
+
+
+
+
+    </div>
+  </div>
+
+
 
 
 </body>
